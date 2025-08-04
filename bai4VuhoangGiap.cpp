1a1266ed3e389a9bf0691530ef34c292622ad8b0
vuhoanggiap <120465721+vuhoanggiap@users.noreply.github.com>
2025-08-04 19:41:23 +0700

    Update bai4VuhoangGiap.cpp

--- bai4VuhoangGiap.cpp
+++ bai4VuhoangGiap.cpp
@@ -9,13 +9,17 @@ int main(){
 	float d = 2.014*a;
 	float e = 2.536*a;
 	if(a <= 50){
-		printf("Ban phai tra so tien: %f",b);
+		printf("Ban phai tra so tien: %.3f",b);
+		printf(" VND");
 	}else if(a > 50 && a<=100){
-		printf("Ban phai tra so tien: %f",c );
+		printf("Ban phai tra so tien: %.3f",c );
+		printf(" VND");
 	}else if(a > 100 && a<200){
-		printf("Ban phai tra so tien: %f",d );
+		printf("Ban phai tra so tien: %.3f",d );
+		printf(" VND");
 	}else if(a = 200){
-		printf("Ban phai tra so tien: %f",e );
+		printf("Ban phai tra so tien: %.3f",e );
+		printf(" VND");
 	};
 	return 0;
 }
