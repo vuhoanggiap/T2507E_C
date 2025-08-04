c833fbd53566e10d95d4f420148c46e2b63b5630
vuhoanggiap <120465721+vuhoanggiap@users.noreply.github.com>
2025-08-04 19:24:14 +0700

    bai 5

---
+++ bai5VuHoangGiap.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	float a , b , c;
+	printf("cho phuong trinh ax^2 + bx + c = 0\n");
+	printf("hay nhap gia tri cua a,b,c de tim gia tri cua x");
+	printf("Ban phai tra so tien: %f",d " nghin dong");
+	scanf("%f",&a);
+	printf("Ban phai tra so tien: %f",d " nghin dong");
+	scanf("%f",&b);
+	printf("Ban phai tra so tien: %f",d " nghin dong");
+	scanf("%f",&c);
+	return 0;
+}
